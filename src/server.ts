08626baed39b68b3c1a21ce293ea37594aa16282
Luiz Ferreira Alves <luiz.ferreira.alves30@gmail.com>
2026-06-21 16:58:10 -0300

    Setting the host

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app'
 import { env } from './env'
 
+const host = ("RENDER" in process.env) ? '0.0.0.0' : 'localhost';
+
 app
   .listen({
     port: env.PORT,
+    host,
   })
   .then(() => {
     console.log('HTTP server running!')
